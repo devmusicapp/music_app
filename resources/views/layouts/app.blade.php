f6b44cc4da81485cc276fbbabc47c83fc65ebd02
subkt2013 <subkt2013@gmail.com>
2021-12-27 23:08:13 +0900

    不要な投稿機能削除

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -41,9 +41,7 @@
                             <li class="nav-item">
                                 <a class="nav-link" href="{{route('recruiting.index')}}">検索</a>
                             </li>                            
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{route('posts.create')}}">投稿</a>
-                            </li>
+
                             <li class="nav-item">
                                 <a class="nav-link" href="#">サポート</a>
                             </li>
@@ -100,12 +98,6 @@
                 <!--Grid column-->
                 <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
                     <h5 class="text-uppercase">検索</h5>
-                </div>
-                <!--Grid column-->
-        
-                <!--Grid column-->
-                <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
-                    <h5 class="text-uppercase mb-0">投稿</h5>
                 </div>
                 <!--Grid column-->
         
